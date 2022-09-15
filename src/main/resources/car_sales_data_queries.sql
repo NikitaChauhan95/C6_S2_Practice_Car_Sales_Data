cdbc3c05a76b1207d7b669a3ca764447f8b9b736
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-09-15 14:25:46 +0530

    created table sales person and added  the details column

--- src/main/resources/car_sales_data_queries.sql
+++ src/main/resources/car_sales_data_queries.sql
@@ -1,3 +1,13 @@
 -- create database CarSales
 CREATE DATABASE IF NOT EXISTS `CarSales`;
 
+-- Create a table sales person in Car sales database
+-- added sales_id, FirstName, City, Commission rate as columns
+--added sales_id as primary key
+USE `Carsales`;
+CREATE TABLE IF NOT EXISTS `Salesperson` (
+    sales_id int not null primary key,
+    first_name varchar(10),
+    city varchar(10),
+    commision_rate int
+);
